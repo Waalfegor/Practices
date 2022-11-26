6a7d63eca44a73b728b4b9f65d7dc951ffb019b5
Waalfegor <100232625+Waalfegor@users.noreply.github.com>
2022-11-26 15:36:43 +0300

    Delete Practice19 directory

--- Practice19/UncorrectINN.java
+++
@@ -1,4 +0,0 @@
-public class UncorrectINN extends RuntimeException
-{
-    public UncorrectINN(String errorMessage){super(errorMessage);}
-}
--- Practice19/myСlient.java
+++
@@ -1,7 +0,0 @@
-package Practice19;
-
-public class myСlient
-{
-    private String INN;
-    private String name, surname, patronymic;
-}
